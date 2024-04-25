74c502dfca968cd69bf2cf987e6e1cf65f176b96
u3612915 <157865509+u3612915@users.noreply.github.com>
2024-04-25 23:09:46 +0800

    upload the part of choose game after animation

---
+++ spider/choose_game.cpp
@@ -0,0 +1,49 @@
+//Coding Requirement: Dynamic memory management: line9
+#include "choose_game.h"
+int choose_game(int flag){
+    //let the player select the game he wants to play (or exit)
+    //input: default flag = 0
+    //output: return 0 to start "Spider Solitaire"; return 1 to start "消消乐"; return 2 to exit
+    
+    int path_size = 1;
+    std::vector<updownchoice> path(path_size);     //use vector to store main interface
+    path[0] = {3, 3, 30, 40, "Poker Carnival", {"--Spider Solitaire--", "--Poker crash--",  "--Exit--"}, 
+    {"cyan", "green", "red", "yellow", "green", "blue", "red"}};
+    //Main interface: Spider Solitaire, match-3 , Exit
+
+    for (int i = 0; i < path_size; i++){    //initialization
+        path[i].mainface = {};
+        path[i].resizemainface();
+        path[i].initialize();
+    }
+    focus_title(path);
+    char i = 'q';
+    system("clear"); 
+    printscreen(path, path_size, flag);
+    
+    system("stty -icanon");
+    system("stty -echo");
+    while (true){
+        i = getchar();
+        if (i == 'w') {
+            path[0].shiftud("up");
+        }
+        else if (i == 's') {
+            path[0].shiftud("down");
+        }
+    
+    else if (i == '\n') {
+            system("stty echo");
+            system("stty icanon");
+            system("clear");
+            return path[0].position;
+            system("stty -icanon");
+            system("stty -echo");
+        }
+        std::cout << std::endl;
+        printscreen(path, path_size); 
+    }
+    system("stty echo");
+    system("stty icanon"); 
+    return 1;
+}
---
+++ spider/choose_game.h
@@ -0,0 +1,8 @@
+#ifndef CHOOSE_GAME_H
+#define CHOOSE_GAME_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include "updownchoice.h"
+int choose_game(int flag);
+#endif
