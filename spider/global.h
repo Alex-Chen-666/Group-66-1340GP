25a13377f2c876256ef06a47800e2ed8d9c051c2
W1ndB10w <158762812+W1ndB10w@users.noreply.github.com>
2024-04-25 23:23:14 +0800

    upload global variables

---
+++ spider/global.h
@@ -0,0 +1,9 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+const std::string top = "╔════╗", side = "║", bottom = "╚════╝";
+const std::string ANSItitle = "\033[", normalize = "\033[0m";
+const std::string color[8] = {"black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"};//几种颜色选择
+//30 31 -37 字体
+//40 47
+//38 48 
+#endif
