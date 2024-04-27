bfe190dd686303980e3925fca9b4f1b2e5c5106e
ChengXiuheng <158027546+srbisupermeni@users.noreply.github.com>
2024-04-28 00:05:23 +0800

    Add files via upload
    Parts for poker crush, including ending animation, input position and model choosing.

---
+++ spider/end_animation.h
@@ -0,0 +1,9 @@
+#ifndef END_ANIMATION_H
+#define END_ANIMATION_H
+#include "headfiles.h"
+#include "column_center_print.h"
+#include <cstdlib>
+void print_not_end(int level, int step, int marks);
+void print_end(int level,int steps,int marks );
+
+#endif
