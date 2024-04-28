7efcf6141988beaf4e43ad2870d61e752eed8da5
u3612915 <157865509+u3612915@users.noreply.github.com>
2024-04-28 10:45:00 +0800

    Update poker_crush.cpp

--- spider/poker_crush.cpp
+++ spider/poker_crush.cpp
@@ -1,39 +1,38 @@
-//Data structures for storing game status :9,10,11
-//Dynamic memory management:10, 94
 #include "poker_crush.h"
 
-extern int GRID_SIZE, steps, marks;
+extern int GRID_SIZE, steps, marks;// global variables defined in "print_grid.cpp" and "quick_game.cpp"
 int state =0;
-void poker_crush(){//主函数
-
-        /*int column_number, reserved_chances, decks_of_card, suit_num, step;//列数，中间发牌次数（中途要不断减一），几副牌（一副52张），花色数
-        int * hidden_position = new int[1]; //Data structures for storing game status, Dynamic memory management 
-        std::vector<std::vector<std::string>> card;//已经发的牌 Data structures for storing game status 
-        std::vector<std::string> savedcard;//发牌堆的牌  Data structures for storing game status */
-        int state2;//flag用于判断是否需要打印提示，0不用，1打印打开失败，2打印没有历史记录
+// main fuction of the game "poker_crush"
+//no input, no output
+void poker_crush(){
+        int state2;
         while (true){
             state2 = choose_mode2(0);
             cout<<"";
-            if (state2 == 0){//教程,之后要继续选择
-                instruction2();   }
-            else if(state2==1){//新游戏
+            if (state2 == 0)
+                instruction2();         // to get into the introduction
+            else if(state2==1){         // to quickly start a new game
                 int level;
                 level = choose_level(0);
-                if(level==3)
+                if(level==4)            //quit the game
                     continue;
-                state2 = quick_game(level);
-                if (state2 == -1){
+                else if(level==3)       //enter the infinite mode
+                    state2 = quick_game(level,true);
+                else                    //start a finite game(with three levels 0 1 2)
+                    state2 = quick_game(level);
+
+            if (state2 == -1){          // player loses or quits the game
                     print_not_end(level, steps, marks);
                     continue;
                 }
-                if (state2 == 2) {
+            if (state2 == 2) {          // player wins the game
                     print_end(level, steps, marks);
                     continue;
                 }
             }
             else if(state2==2){
                 std::cout<<"\033[?25h";
-                return ;             //直接退出
+                return ;                //go back to the previous page
             } 
             
         
