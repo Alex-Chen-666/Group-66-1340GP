5f2af8126e1de016f2f4a108dc46603f8ec85e37
u3612915 <157865509+u3612915@users.noreply.github.com>
2024-04-28 10:55:56 +0800

    Update print_grid.h

--- spider/print_grid.h
+++ spider/print_grid.h
@@ -1,6 +1,5 @@
 #ifndef PRINTGRID
 #define PRINTGRID
-
 #include "headfiles.h"
 #include "column_center_print.h"
 using namespace std;
@@ -8,8 +7,6 @@ using namespace std;
 string createcolor(const string& foreground, const string& background = "");
 void initGrid();
 void printunit(int i, int j, int selecting_mood = 0);
-void printGrid();
+void printGrid(bool endless=false);
 void printInvalid(char m);
-
-
 #endif
