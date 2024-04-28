3041a2d5416238e3c660cb7ef8681548e0375983
W1ndB10w <158762812+W1ndB10w@users.noreply.github.com>
2024-04-28 17:57:18 +0800

    Update choose_history.cpp

--- spider/choose_history.cpp
+++ spider/choose_history.cpp
@@ -1,5 +1,5 @@
 ////Coding Requirement: Data structures for storing game status: line16
-//feature: can see the history with rolling up and down
+//feature: The player can scroll up and down the history records
 #include "choose_history.h"
 #include "process_record.h"
 int choose_history(int total_file_index, int max_record_num, std::string &record_name){
